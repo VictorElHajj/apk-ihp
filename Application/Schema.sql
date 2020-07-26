c052092045eaef97ede39b2d3e21154b8391ab1e
VictorElHajj <hajjvictor@live.se>
2020-07-26 18:12:44 +0200

    Add articles table

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -1,1 +1,12 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE articles (
+    id INT PRIMARY KEY NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    price REAL NOT NULL,
+    volume REAL NOT NULL,
+    item_group TEXT NOT NULL,
+    style TEXT NOT NULL,
+    abv REAL NOT NULL,
+    availability TEXT NOT NULL,
+    apk DOUBLE PRECISION NOT NULL
+);
