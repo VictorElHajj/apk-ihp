e13079d484f78807fd0b69596c6e2fc1e936e440
VictorElHajj <hajjvictor@live.se>
2020-07-30 20:46:21 +0200

    Move layout into default layout

--- Web/View/Layout.hs
+++ Web/View/Layout.hs
@@ -19,15 +19,27 @@ defaultLayout inner = H.docTypeHtml ! A.lang "en" $ [hsx|
     {stylesheets}
     {scripts}
 
-    <title>App</title>
+    <title>APK</title>
 </head>
 <body>
     <div class="container mt-4">
+        <nav class="navbar navbar-expand navbar-dark bg-dark shadow">
+            <a class="navbar-brand" href="#">APK</a>
+            <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Hem <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Om</a>
+            </li>
+            </ul>
+        </nav>
         {renderFlashMessages}
         {inner}
     </div>
 </body>
 |]
+
 
 stylesheets = do
     when (isDevelopment FrameworkConfig.environment) [hsx|
@@ -58,7 +70,7 @@ scripts = do
 metaTags = [hsx|
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <meta property="og:title" content="App"/>
+    <meta property="og:title" content="Alkol per krona"/>
     <meta property="og:type" content="website"/>
     <meta property="og:url" content="TODO"/>
     <meta property="og:description" content="TODO"/>
